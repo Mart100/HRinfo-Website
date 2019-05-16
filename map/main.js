8d600c36a7e8fe091f00af4eda6f467c4a183d48
Marto_0 <martvanenck1@gmail.com>
2019-05-16 22:11:10 +0200

    map update

--- map/main.js
+++ map/main.js
@@ -1,5 +1,7 @@
 let map
-
+let imageOverlayMap,
+    imageOverlayMapNames,
+    imageOverlayChestLocations
 $(() => {
 
   map = L.map('map', {
@@ -15,7 +17,40 @@ $(() => {
   let bounds = new L.LatLngBounds(southWest, northEast)
 
 
-  L.imageOverlay('map.jpg', bounds).addTo(map)
+  imageOverlayMap = L.imageOverlay('backgroundMap.jpg', bounds).addTo(map)
+
+  imageOverlayMapNames = L.imageOverlay('mapNames.png', bounds).addTo(map)
+
+  imageOverlayChestLocations = L.imageOverlay('chestLocations.png', bounds).addTo(map)
 
 
+
+})
+
+// controlPanel
+$(() => {
+
+  // overlays
+
+  // background map
+  $('#controlPanel #overlays #background').on('click', () => {
+    let checked = $('#controlPanel #overlays #background').is(':checked')
+    if(checked) imageOverlayMap.setOpacity(1)
+    else imageOverlayMap.setOpacity(0)
+  })
+
+  // Place Names
+  $('#controlPanel #overlays #placeNames').on('click', () => {
+    let checked = $('#controlPanel #overlays #placeNames').is(':checked')
+    if(checked) imageOverlayMapNames.setOpacity(1)
+    else imageOverlayMapNames.setOpacity(0)
+  })
+
+  
+  // Chest Locations
+  $('#controlPanel #overlays #chestLocations').on('click', () => {
+    let checked = $('#controlPanel #overlays #chestLocations').is(':checked')
+    if(checked) imageOverlayChestLocations.setOpacity(1)
+    else imageOverlayChestLocations.setOpacity(0)
+  })
 })
