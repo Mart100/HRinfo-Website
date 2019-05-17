910ad0628e61779ad5f82baeb4013e144495450f
Marto_0 <martvanenck1@gmail.com>
2019-05-17 14:16:28 +0200

    get clans from database

--- clans/main.js
+++ clans/main.js
@@ -1,10 +1,22 @@
-$(() => {
-  $.getJSON('./clans.json', (data) => {
-    for(let clan of data) addClan(clan)
+let firestore = firebase.firestore()
 
-    $('#clans').append(`<div style="color: white;" class="clan">DM me on discord (Marto_0#1978) to have your own clan added here!</div>`)
+// on page load
+$(() => {
+
+  // read clans from database
+  firestore.collection("clans").get().then((querySnapshot) => {
+    let clans = []
+    querySnapshot.forEach((doc) => {
+      let data = doc.data()
+      clans.push(data)
+    })
+
+    for(let clan of clans) addClan(clan)
   })
+
+  $('#clans').append(`<div style="color: white; text-align: center;" class="clan"><span style="position: relative; top: 25px;">To have your own clan added here, Invite this discord bot to you clan discord server!<br><a href="https://discordapp.com/oauth2/authorize?client_id=578905640181825556&permissions=67585&scope=bot">Bot invite</a></span></div>`)
 })
+
 
 function addClan(clan) {
   let html = `
@@ -12,7 +24,7 @@ function addClan(clan) {
     <img src="${clan.image}"/>
     <div class="name">${clan.name}</div>
     <div class="desc">${clan.desc}</div>
-    <a class="discord" href="${clan.discord}"><img src="https://i.imgur.com/2lEoaBb.png"/></a>
+    <a class="discord" href="https://discord.gg/${clan.invite}"><img src="https://i.imgur.com/2lEoaBb.png"/></a>
   </div>
   `
   $('#clans').append(html)
