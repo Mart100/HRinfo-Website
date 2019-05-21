7d0c3bc40e2fdde523fd837cbe9ec44858d52ebc
Marto_0 <martvanenck1@gmail.com>
2019-05-21 22:15:00 +0200

    start of profile

--- profile/main.js
+++ profile/main.js
@@ -1,39 +1,44 @@
-$(() => {
-  let discordID = getCookie('discord')
+let players
 
-  if(discordID == "") loginDiscord()
+$(() => {
+  // get players
+  $.get('https://hrinfo-api.herokuapp.com/players', (data) => { players = data })
+
+  // on search enter
+  $('#search').on('keypress', (event) => {
+    if(event.key == 'Enter') {
+      let text = $('#search').val()
+      let player = searchPlayer(text)
+      $('#search').val('')
+      console.log(player)
+
+      // assign data
+      $('#top #username').html(player.username)
+
+
+      $('#profile #bottom').html('')
+      $('#profile #bottom').append('<br>')
+      $('#profile #bottom').append(`<span>Clan: ${player.clan}</span><br>`)
+      $('#profile #bottom').append(`<span>Points: ${player.points}</span><br>`)
+      $('#profile #bottom').append(`<span>Division: ${player.division}</span><br>`)
+
+
+
+
+      // appear
+      $('#search').animate({'left': '1%', 'width': '350px'}, 500, () => {
+        $('#profile').fadeIn(500)
+      })
+    }
+  })
 })
 
 
-function loginDiscord() {
-  let link = 'https://discordapp.com/api/oauth2/authorize?client_id=578905640181825556&redirect_uri=https%3A%2F%2Fhrinfo.xyz%2Fprofile&response_type=code&scope=identify'
-  let html = `
-  <a href="${link}" style="text-decoration: none;"><div id="login">Log in with discord</div></a>
-  `
-  $('#profile').html(html)
 
-  // if joined with params
-  let params = window.location.search.substr(1)
-  if(params != '') {
-    let code = params.replace('code=', '')
-    const fragment = new URLSearchParams(window.location.hash.slice(1))
-    console.log(fragment)
-    if(fragment.has("access_token")) {
-      const accessToken = fragment.get("access_token");
-      const tokenType = fragment.get("token_type");
-
-      fetch('https://discordapp.com/api/users/@me', {
-        headers: {
-          authorization: `${tokenType} ${accessToken}`
-        }
-      })
-        .then(res => res.json())
-        .then(response => {
-          const { username, discriminator } = response;
-          console.log(`${username}#${discriminator}`)
-        })
-        .catch(console.error);
-
-    }
+function searchPlayer(text) {
+  for(let playerID in players) {
+    let player = players[playerID]
+    if(player.username == text) return player
+    if(player.id == text) return player
   }
-}  
+}
