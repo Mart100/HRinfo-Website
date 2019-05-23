b74b1bfae830c08d2fc7f807c7ff2fd756d0fcf9
Marto_0 <martvanenck1@gmail.com>
2019-05-23 18:43:29 +0200

    clan profiles. and login fixed

--- clans/main.js
+++ clans/main.js
@@ -1,10 +1,21 @@
+let clans
+let clan
+let players
+let divisions
+
 // on page load
 $(() => {
+  
+  let params = window.location.search.substr(1)
+  let clanParam = params.replace('clan=', '')
+
+
 
   // read clans from database
   $.get('https://hrinfo-api.herokuapp.com/clans', (data) => { 
-    let clans = data
+    clans = data
     for(let i in clans) addClan(clans[i])
+    if(clanParam != '') showClanFull(clanParam)
   })
 
   let text = `
@@ -19,7 +30,7 @@ $(() => {
 
 function addClan(clan) {
   let html = `
-  <div class="clan" id="clan-${clan.name}">
+  <div class="clan" id="clan-${clan.id}">
     <img src="${clan.image}"/>
     <span class="name">${clan.name}</span>
     <span class="desc">${clan.desc}</span>
@@ -31,6 +42,93 @@ function addClan(clan) {
   `
   $('#clans').append(html)
 
+  $(`#clan-${clan.id}`).on('click', () => {
+    window.location += `?clan=${clan.id}`
+  })
+
   if(clan.invite == 'none') $(`#clan-${clan.name} .discord`).css('visibility', 'hidden')
 
+}
+
+//
+function showClanFull(clanID) {
+  clan = clans[clanID]
+  let html = `
+    <div id="profile">
+      <div id="top">
+        <img id="image" src="${clan.image}" />
+        <div id="name">${clan.name}</div>
+        <div id="joinclan">Join clan</div>
+      </div>
+      <div id="navC">
+        <span class="overview" onclick="showOverview()">Overview</span>
+        <span class="members" onclick="showMembers()" >Members</span>
+      </div>
+      <div id="bottom">
+
+      </div>
+    </div>
+  `
+  $('#clans').remove()
+  $('body').append(html)
+  showOverview()
+
+  $('#joinclan').on('click', () => {
+    
+  })
+
+}
+function showMembers() {
+
+  // if players undefined
+  if(players == undefined) {
+    $.get('https://hrinfo-api.herokuapp.com/players', (data) => { 
+      players = data
+      showMembers()
+    })
+    return
+  }
+
+  // if divisions undefined
+  if(divisions == undefined) {
+    $.get('https://hrinfo-api.herokuapp.com/divisions', (data) => { 
+      divisions = data
+      showMembers()
+    })
+    return
+  }
+
+
+  $('#navC span').removeClass('selected')
+  $('#navC .members').addClass('selected')
+  $('#bottom').html('')
+  $('#bottom').append('')
+
+  // add players
+
+  let sortedPlayers = Object.values(players).sort((a, b) => b.points-a.points)
+  let filteredPlayers = sortedPlayers.filter((p) => p.clan == clan.name )
+
+  for(let i in filteredPlayers) {
+    let player = filteredPlayers[i]
+    let html = `
+    <div class="player" id="${player.id}">
+      <span class="placement">#${Number(i)+1}: </span>
+      <span class="name">${player.username}</span>
+      <span class="points">Points: ${player.points}</span>
+      <span class="division">Division:</span><img class="divisionIMG" src="${divisions[player.division].image}" />
+    </div>
+    `
+    $('#bottom').append(html)
+  }
+}
+
+function showOverview() {
+  $('#navC span').removeClass('selected')
+  $('#navC .overview').addClass('selected')
+  $('#bottom').html('')
+  $('#bottom').append(`<br>`)
+  $('#bottom').append(`<span>Points: ${clan.points}</span><br>`)
+  $('#bottom').append(`<span>Discord members: ${clan.discordMemberCount}</span><br>`)
+  $('#bottom').append(`<span>Members: ${clan.members.length}</span>`)
 }
