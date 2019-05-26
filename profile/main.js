aa7e7cfa0ca9f7fbd32079895411c7e3b540f80c
Marto_0 <martvanenck1@gmail.com>
2019-05-26 15:12:21 +0200

    Profile update

--- profile/main.js
+++ profile/main.js
@@ -9,14 +9,42 @@ async function onload() {
   // see if player in params
   let params = window.location.search.substr(1)
   let playerParam = params.replace('player=', '')
-  if(playerParam != undefined) viewPlayer(playerParam)
+  if(playerParam != "") viewPlayer(playerParam)
+
+  // see if token cookie
+  let tokenCookie = getCookie('token')
+  console.log(tokenCookie)
+  if(tokenCookie != "") {
+    let id = tokenCookie.split('-')[0]
+    console.log(id)
+    viewPlayer(id)
+  }
 
   // on search enter
   $('#search').on('keypress', (event) => {
     if(event.key == 'Enter') viewPlayer($('#search').val())
+    else searchmodeON()
+  })
+
+}
+
+function searchmodeOFF() {
+  if($('#search').hasClass('searching') == false) return
+  $('#search').removeClass('searching')
+  $('#search').animate({'left': '1%', 'width': '350px'}, 300, () => {
+    $('#profile').animate({'margin-top': '50px'}, 200)
+    $('#profile').fadeIn(300)
   })
 }
 
+function searchmodeON() {
+  if($('#search').hasClass('searching')) return
+  $('#search').addClass('searching')
+  let leftCalc = window.innerWidth/2 - 250
+  $('#profile').animate({'margin-top': '150px'}, 200, () => {
+    $('#search').animate({'left': leftCalc, 'width': '500px'}, 300)
+  })
+}
 
 function viewPlayer(text) {
   let player = searchPlayer(text)
@@ -35,15 +63,22 @@ function viewPlayer(text) {
 
 
   // appear
-  $('#search').animate({'left': '1%', 'width': '350px'}, 500, () => {
-    $('#profile').fadeIn(500)
-  })
+  searchmodeOFF()
 }
 
 function searchPlayer(text) {
+  let possibilities = []
   for(let playerID in players) {
     let player = players[playerID]
     if(player.username == text) return player
     if(player.id == text) return player
+    let score = player.username.score(text)
+    possibilities.push([player.id, score])
   }
+
+  // sort possibilities
+  possibilities.sort((a, b) => b[1]-a[1])
+
+  let highestPossiblePlayer = players[possibilities[0][0]]
+  return highestPossiblePlayer
 }
