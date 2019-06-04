ef50e0634fe5873a3db8b15ab5c99329aab1047f
Marto_0 <martvanenck1@gmail.com>
2019-06-04 14:29:36 +0200

    tournaments page

--- tournaments/main.js
+++ tournaments/main.js
@@ -14,8 +14,9 @@ function addTournament(tournament) {
   <div class="tournament" id="${tournament.id}">
     <span>Name: ${tournament.name}</span><br>
     <span>Host: ${tournament.host}</span><br>
-    <span>Contesters: ${tournament.players.length}</span><br>
+    <span>Contesters: ${Object.keys(tournament.players).length}</span><br>
     <span>Status: ${tournament.status}</span><br>
+    <span><a href="https://challonge.com/${tournament.name}">Brackets</a></span><br>
   </div>
   `
 
